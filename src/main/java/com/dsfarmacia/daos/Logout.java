ecd382a70c0d837a0d3e347b21d330ab4e3ebd58
Nizath <is687045@iteso.mx>
2015-11-26 13:15:41 -0600

    Logout stylysed

--- src/main/java/com/dsfarmacia/daos/Logout.java
+++ src/main/java/com/dsfarmacia/daos/Logout.java
@@ -1,48 +1,64 @@
 package com.dsfarmacia.daos;
 
+import com.dsfarmacia.db.Conexion;
+import com.dsfarmacia.db.IConexion;
+
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Calendar;
-import com.dsfarmacia.db.*;
+
+
+
 
 public class Logout {
-	private IConexion Conexion;
-	private Statement stmt;
-	public Logout(){
-		setConexion(new Conexion());
-	}
-	public boolean connect(String user, String date){
-		String SQL = "INSERT INTO [FarmaciaWeb].[dbo].[Log]  Values('"
-				+ user + "','" +date + "'" + ",'" + getCurrentDate() + "');";
-		boolean salida=true;
-		try {
-			setStmt(getConexion().getCon().createStatement());
-			salida= getStmt().execute(SQL);
-			CloseConection();
-		//	System.out.println(rs.getString(1));	
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return salida;
-		
-	}
-	public String getCurrentDate(){
-		return new java.sql.Date(Calendar.getInstance().getTime().getTime()).toString();
-	}
-	public IConexion getConexion() {
-		return Conexion;
-	}
-	public void setConexion(IConexion conexion) {
-		Conexion = conexion;
-	}
-	public Statement getStmt() {
-		return stmt;
-	}
-	public void setStmt(Statement stmt) {
-		this.stmt = stmt;
-	}
-	public  void CloseConection() throws SQLException {
-		Conexion.closeCon();
-	}
+  private IConexion conexion;
+  private Statement stmt;
+  
+  public Logout() {
+    setConexion(new Conexion());
+  }
+  /**.
+  * @param user username
+  * @param date last date
+  * @return true if connected
+  */
+  public boolean connect(String user, String date) {
+    String sql = "INSERT INTO [FarmaciaWeb].[dbo].[Log]  Values('"
+        + user + "','" + date + "'" + ",'" + getCurrentDate() + "');";
+    boolean salida = true;
+    try {
+      setStmt(getConexion().getCon().createStatement());
+      salida = getStmt().execute(sql);
+      closecon();
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return salida;
+
+  }
+  
+  public String getCurrentDate() {
+    return new java.sql.Date(Calendar.getInstance().getTime().getTime()).toString();
+  }
+  
+  public IConexion getConexion() {
+    return conexion;
+  }
+  
+  public void setConexion(IConexion conexion) {
+    this.conexion = conexion;
+  }
+  
+  public Statement getStmt() {
+    return stmt;
+  }
+  
+  public void setStmt(Statement stmt) {
+    this.stmt = stmt;
+  }
+  
+  public  void closecon() throws SQLException {
+    conexion.closeCon();
+  }
 }
