77760328f4995767f5a4cfb5994724bfff3b4855
Nizath <is687045@iteso.mx>
2015-11-26 11:48:23 -0600

    bean unidad medida checkstyled

--- src/main/java/com/dsfarmacia/beans/BeanUnidadMedida.java
+++ src/main/java/com/dsfarmacia/beans/BeanUnidadMedida.java
@@ -1,30 +1,29 @@
 package com.dsfarmacia.beans;
 
 public class BeanUnidadMedida {
-	
-	private int unidadmedidaId;
-	private String unidadmedidanombre;
-	
-	public int getUnidadmedidaId() {
-		return unidadmedidaId;
-	}
-	public void setUnidadmedidaId(int unidadmedidaId) {
-		this.unidadmedidaId = unidadmedidaId;
-	}
-	public String getUnidadmedidanombre() {
-		return unidadmedidanombre;
-	}
-	public void setUnidadmedidanombre(String unidadmedidanombre) {
-		this.unidadmedidanombre = unidadmedidanombre;
-	}
-	
-	@Override
-	public String toString() {
-		return "BeanUnidadMedida [unidadmedidaId=" + unidadmedidaId + ", unidadmedidanombre=" + unidadmedidanombre
-				+ "]";
-	}
-	
-	
-	
+  private int unidadmedidaId;
+  private String unidadmedidanombre;
+  
+  public int getUnidadmedidaId() {
+    return unidadmedidaId;
+  }
+  
+  public void setUnidadmedidaId(int unidadmedidaId) {
+    this.unidadmedidaId = unidadmedidaId;
+  }
+  
+  public String getUnidadmedidanombre() {
+    return unidadmedidanombre;
+  }
+  
+  public void setUnidadmedidanombre(String unidadmedidanombre) {
+    this.unidadmedidanombre = unidadmedidanombre;
+  }
+
+  @Override
+  public String toString() {
+    return "BeanUnidadMedida [unidadmedidaId=" + unidadmedidaId + ","
+      + " unidadmedidanombre=" + unidadmedidanombre + "]";
+  }
 
 }
