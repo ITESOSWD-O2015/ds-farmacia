77a96ceece919540eb9456af2042eb8417ca5a30
Nizath <is687045@iteso.mx>
2015-11-26 12:22:34 -0600

    Iconexion checkstylised

--- src/main/java/com/dsfarmacia/db/IConexion.java
+++ src/main/java/com/dsfarmacia/db/IConexion.java
@@ -5,11 +5,13 @@ import java.sql.SQLException;
 
 public interface IConexion {
 
-	public boolean crearConexion();
-	public boolean cerrarConexion();
-	
-	public void setCon(Connection con);
-	public Connection getCon();
-	public void closeCon() throws SQLException;
-	
+  public boolean crearConexion();
+  
+  public boolean cerrarConexion();
+  
+  public void setCon(Connection con);
+  
+  public Connection getCon();
+  
+  public void closeCon() throws SQLException;
 }
